8184fc096217004708bab13c80eba612f29e00b1
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-08 11:26:28 +0900

    count 조절 액션 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -12,33 +12,16 @@ public class UsrHomeController {
 	public UsrHomeController() { 
 		count = -1;
 	}
-	@RequestMapping("/usr/home/main")
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	}
-	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
-	}
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		count++;
+	public int getCount() {
 		return count;
 	}
-	@RequestMapping("/usr/home/main5")
+	
+	@RequestMapping("/usr/home/dosetCount")
 	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "count의 값이 0으로 초기화 되었습니다.";
-	}
-
+	public String dosetCount(int count) {
+		this.count = count;
+		return "count의 값이" + this.count + "초기화 되었습니다.";
+	}	
 }
