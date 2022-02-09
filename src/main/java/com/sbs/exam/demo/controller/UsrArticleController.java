a280bf184979e5fb1ede0e8ea8ac483315475efc
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-09 10:46:52 +0900

    테스트용 게시물 10개 자동 생성

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -11,19 +11,32 @@ import com.sbs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	
 	private int articlesLastId;
-	
 	private List<Article> articles;
 	
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
+	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			articlesLastId = id;
+		}		
+	}
+	
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articlesLastId + 1;
+		int id = articlesLastId +1;
 		Article article = new Article(id, title, body);
 		
 		articles.add(article);
@@ -31,10 +44,17 @@ public class UsrArticleController {
 		
 		return article;
 	}
+	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
 	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		return id + "번 게시물을 삭제하였습니다.";
+	}
 }
-
