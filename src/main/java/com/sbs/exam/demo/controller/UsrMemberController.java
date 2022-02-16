ce427011ced54aa93cdac522485f629fd738211b
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-17 00:48:12 +0900

    Ut.empty 함수 도입

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.exam.demo.service.MemberService;
+import com.sbs.exam.demo.util.Ut;
 import com.sbs.exam.demo.vo.Member;
 
 @Controller
@@ -19,35 +20,35 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		
-		if (loginId == null || loginId.trim().length()==0) {
+
+		if (Ut.empty(loginId)) {
 			return "loginId(을)를 입력해주세요.";
 		}
-		
-		if (loginPw == null || loginPw.trim().length()==0) {
+
+		if (Ut.empty(loginPw)) {
 			return "loginPw(을)를 입력해주세요.";
 		}
-		
-		if (nickname == null || nickname.trim().length()==0) {
+
+		if (Ut.empty(nickname)) {
 			return "nickname(을)를 입력해주세요.";
 		}
-		
-		if (cellphoneNo == null || cellphoneNo.trim().length()==0) {
+
+		if (Ut.empty(cellphoneNo)) {
 			return "cellphoneNo(을)를 입력해주세요.";
 		}
-		
-		if (email == null || email.trim().length()==0) {
+
+		if (Ut.empty(email)) {
 			return "email(을)를 입력해주세요.";
 		}
-		
+
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
+
 		if (id == -1) {
 			return "해당 로그인 아이디는 이미 사용중입니다.";
 		}
-		
+
 		Member member = memberService.getMemberById(id);
-		
+
 		return member;
 	}
 }
