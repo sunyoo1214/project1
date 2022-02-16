e5ba31705514a7665f689a235a5028c3444ba80e
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-17 00:14:41 +0900

    회원가입 시 중복아이디 체크

--- src/main/java/com/sbs/exam/demo/service/MemberService.java
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -7,16 +7,25 @@ import com.sbs.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
-
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		Member oldMember = getMemberByLoginId(loginId);
+		
+		if ( oldMember != null ) {
+			return -1;
+		}
+		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLastInsertId();
+	}
+
+	private Member getMemberByLoginId(String loginId) {
+		return memberRepository.getMemberByLoginId(loginId);
 	}
 
 	public Member getMemberById(int id) {
