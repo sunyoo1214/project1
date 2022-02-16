414acd258630a6f1d0af69a6591d5d45f03db1ab
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-16 23:48:52 +0900

    회원가입 완료 후 생성된 회원 보여줌

--- src/main/java/com/sbs/exam/demo/service/MemberService.java
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.sbs.exam.demo.service;
 
 import org.springframework.stereotype.Service;
+
 import com.sbs.exam.demo.repository.MemberRepository;
+import com.sbs.exam.demo.vo.Member;
 
 @Service
 public class MemberService {
@@ -12,7 +14,12 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);	
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		return memberRepository.getLastInsertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
 	}
 }
