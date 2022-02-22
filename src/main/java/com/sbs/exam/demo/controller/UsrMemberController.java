31a4785d0201e3047de97bb8f6139479b8c57ca5
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-22 22:04:22 +0900

    로그아웃 기능 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -19,7 +19,24 @@ public class UsrMemberController {
 		this.memberService = memberService;
 	}
 
-
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public ResultData<Member> doLogout(HttpSession httpSession) {
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+		
+		if(isLogined) {
+			return ResultData.from("S-1", "이미 로그아웃 상태입니다.");
+		}
+		
+		httpSession.removeAttribute("loginedMemberId");
+		
+		return ResultData.from("S-2", "로그아웃 되었습니다.");
+	}	
+	
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
 	public ResultData<Member> doLogin(HttpSession httpSession, String loginId, String loginPw) {
