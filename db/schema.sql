f76e2bf1b1a8a8aae64cd2790e8a6781b822b5b7
sunyoo1214 <sunyoo1214@gmail.com>
2022-02-25 23:49:04 +0900

    게시물 테이블에 회원번호 칼럼 추가

--- db/schema.sql
+++ db/schema.sql
@@ -32,25 +32,25 @@ title = '제목3',
 `body`= '내용3';
 
 # 회원 테이블 생성
-create table `member` (
-    id int(10) unsigned not null primary key auto_increment,
-    regDate datetime not null,
-    updateDate datetime not null,
-    loginId char(20) not null,
-    loginPw char(20) not null,
-    authLevel smallint(2) unsigned default 3 comment '권한레벨(3=일반, 7=관리자)',
-    `name` char(20) not null,
-    nickname char(20) not null,
-    cellphoneNo char(20) not null,
-    email char(50) not null,
-    delStatus tinyint(1) unsigned not null default 0 comment '탈퇴여부(0=탈퇴전, 1=탈퇴)',
-    delDate DATETIMe comment '탈퇴날짜'
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(20) NOT NULL,
+    authLevel SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨(3=일반, 7=관리자)',
+    `name` CHAR(20) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    cellphoneNo CHAR(20) NOT NULL,
+    email CHAR(50) NOT NULL,
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
+    delDate DATETIME COMMENT '탈퇴날짜'
 );
 
 # 회원, 테스트 데이터 생성(관리자 회원)
-insert into `member`
-set regDate = now(),
-updateDate = now(),
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
 loginId = 'admin',
 loginPw = 'admin',
 authLevel = 7,
@@ -60,9 +60,9 @@ cellphoneNo = '01011111111',
 email = 'sunyoo1214@gmail.com';
 
 # 회원, 테스트 데이터 생성(일반 회원)
-insert into `member`
-set regDate = now(),
-updateDate = now(),
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
 loginId = 'user1',
 loginPw = 'user1',
 `name` = '사용자1',
@@ -70,9 +70,9 @@ nickname = '사용자1',
 cellphoneNo = '01011111111',
 email = 'bigi0511@naver.com';
 
-insert into `member`
-set regDate = now(),
-updateDate = now(),
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
 loginId = 'user2',
 loginPw = 'user2',
 `name` = '사용자2',
@@ -80,4 +80,16 @@ nickname = '사용자2',
 cellphoneNo = '01011111111',
 email = 'lovely1214@naver.com';
 
-select * from `member`
+# 테이블 속 칼럼과 데이터 확인
+SELECT * FROM `member`;
+
+# 게시물 테이블에 회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
+
+# 테이블 속 칼럼과 데이터 확인
+SELECT * FROM `article`;
+
+# 기존 게시물의 작성자를 2번으로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
