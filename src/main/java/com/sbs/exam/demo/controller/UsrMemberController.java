1f0b41e43e70790d386193f17da08ced67d54d15
sunyoo1214 <sunyoo1214@gmail.com>
2022-03-16 23:26:17 +0900

    로그아웃 후에 자바스크립트로 후처리

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -1,7 +1,6 @@
 package com.sbs.exam.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,16 +60,16 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doLogout")
 	@ResponseBody
-	public ResultData doLogout(HttpServletRequest req) {
+	public String doLogout(HttpServletRequest req) {
 		Rq rq = (Rq) req.getAttribute("rq");
 		
-		if (rq.isLogined()) {
-			return ResultData.from("S-1", "이미 로그아웃 상태입니다.");
+		if (!rq.isLogined()) {
+			return Ut.jsHistoryBack("이미 로그아웃 상태입니다.");
 		}
 
 		rq.logout();
 
-		return ResultData.from("S-2", "로그아웃 되었습니다.");
+		return Ut.jsReplace("로그아웃 되었습니다.", "/");
 	}
 	
 	@RequestMapping("/usr/member/login")
