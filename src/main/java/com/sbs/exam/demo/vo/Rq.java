ce226cec4e42a618c86d8adb077031e150a0275b
sunyoo1214 <sunyoo1214@gmail.com>
2022-03-24 13:08:03 +0900

    BeforeActionInterceptor에서 Rq객체를 좀 더 확실하게 생성하도록 유도

--- src/main/java/com/sbs/exam/demo/vo/Rq.java
+++ src/main/java/com/sbs/exam/demo/vo/Rq.java
@@ -88,4 +88,10 @@ public class Rq {
 	public String jsReplace(String msg, String uri) {
 		return Ut.jsReplace(msg, uri);
 	}
+
+	// 이 메서드는 Rq 객체가 자연스럽게 생성되도록 유도하는 역할이다.
+	// 지우면 안되고, 편의를 위해 BeforeActionInterceptor에서 꼭 호출해줘야 한다.
+	public void initOnBeforeActionInterceptor() {
+	
+	}
 }
