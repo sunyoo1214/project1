7328a617e1aa589876529ad784a2e3381c07ca65
sunyoo1214 <sunyoo1214@gmail.com>
2022-04-08 13:30:53 +0900

    댓글 작성 replaceUri 오류 해결

--- src/main/java/com/sbs/exam/demo/controller/UsrReplyController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrReplyController.java
@@ -24,36 +24,70 @@ public class UsrReplyController {
 		this.articleService = articleService;
 		this.rq = rq;
 	}
-	
+
 	@RequestMapping("/usr/reply/modify")
 	public String modify(int id, String replaceUri, Model model) {
-		if(Ut.empty(id)) {
+		if (Ut.empty(id)) {
 			return rq.jsHistoryBack("id(을)를 입력해주세요.");
 		}
-		
+
 		Reply reply = replyService.getForPrintReply(rq.getLoginedMember(), id);
-		
-		if(reply == null) {
+
+		if (reply == null) {
 			return rq.historyBackJsOnView(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
 		}
-		
-		if(reply.isExtra__actorCanModify() == false) {
+
+		if (reply.isExtra__actorCanModify() == false) {
 			return rq.historyBackJsOnView(Ut.f("%d번 댓글을 수정할 권한이 없습니다.", id));
 		}
-		
+
 		String relDataTitle = null;
-		
+
 		switch (reply.getRelTypeCode()) {
 		case "article":
 			Article article = articleService.getArticle(reply.getRelId());
 			relDataTitle = article.getTitle();
 		}
-		
+
 		model.addAttribute("relDataTitle", relDataTitle);
 		model.addAttribute("reply", reply);
-		
+
 		return "usr/reply/modify";
 	}
+
+	@RequestMapping("/usr/reply/doModify")
+	@ResponseBody
+	public String doModify(int id, String body, String replaceUri) {
+		if (Ut.empty(id)) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
+		}
+
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMember(), id);
+
+		if (reply == null) {
+			return rq.historyBackJsOnView(Ut.f("%d번 댓글은 존재하지 않습니다.", id));
+		}
+
+		if (reply.isExtra__actorCanModify() == false) {
+			return rq.historyBackJsOnView(Ut.f("%d번 댓글을 수정할 권한이 없습니다.", id));
+		}
+
+		if (Ut.empty(body)) {
+			return rq.jsHistoryBack("body(을)를 입력해주세요.");
+		}
+
+		ResultData modifyReplyRd = replyService.modifyReplyRd(id, body);
+
+		if (Ut.empty(replaceUri)) {
+			switch (reply.getRelTypeCode()) {
+			case "article":
+				replaceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+		return rq.jsReplace(modifyReplyRd.getMsg(), replaceUri);
+	}
+	
 
 	@RequestMapping("/usr/reply/doWrite")
 	@ResponseBody
@@ -76,11 +110,12 @@ public class UsrReplyController {
 		if (Ut.empty(replaceUri)) {
 			switch (relTypeCode) {
 			case "article":
-				replaceUri = Ut.f("..article/detail?id=%d", relId);
+				replaceUri = Ut.f("../article/detail?id=%d", relId);
 				break;
 			}
 
 		}
+		
 		return rq.jsReplace(writeReplyRd.getMsg(), replaceUri);
 	}
 
