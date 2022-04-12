74594722dc8c5bc97795c4968c7ab7b4f9133e9c
sunyoo1214 <sunyoo1214@gmail.com>
2022-04-12 11:36:14 +0900

    회원정보 수정 전, 비밀번호 확인

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -116,4 +116,16 @@ public class UsrMemberController {
 	public String showCheckPassword() {
 		return "usr/member/checkPassword";
 	}
+	
+	@RequestMapping("/usr/member/doCheckPassword")
+	@ResponseBody
+	public String doCheckPassword(String loginPw, String replaceUri) {
+		if(Ut.empty(loginPw)) {
+			return rq.jsHistoryBack("loginPw(을)를 입력해주세요.");
+		}
+		if (rq.getLoginedMember().getLoginPw().equals(loginPw) == false) {
+			return rq.jsHistoryBack("비밀번호가 일치하지 않습니다.");
+		}
+		return rq.jsReplace("", replaceUri);
+	}
 }
